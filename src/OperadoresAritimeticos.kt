83ac67892db6ae362da83a1202faffd6386f7058
Lucas Gabriel <70025847+luk-z0@users.noreply.github.com>
2022-04-09 17:39:44 -0300

    commit1

---
+++ src/OperadoresAritimeticos.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // + , - , * , %
+
+    var soma = 1 + 1
+    var sub = 1 - 1
+    var mult = 1 * 1
+    var rest = 1 % 1
+
+    print("soma : $soma \nsubtração: $sub\nmultiplicação: $mult\nresto da divisão: $rest")
+
+}
