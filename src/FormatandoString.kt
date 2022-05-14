f9a8be721adab19103b9b739a87b03c999ccf48f
Lucas Gabriel <70025847+luk-z0@users.noreply.github.com>
2022-05-13 22:59:43 -0300

    Delete src directory

--- src/FormatandoString.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    val nome = "Lucas"
-    val sobrenome = "Gabriel"
-    val idade = 20
-    println("Olá, $nome $sobrenome ,\n idade : $idade"  )
-    val strg = "um" + "txt" + "longo"
-
-}
